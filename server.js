05e356f5acc2da72bc8e64d1aeb5122f99c94c2a
yousufhasany <hyousuf266@gmail.com>
2026-04-30 02:42:10 +0600

    remove ss

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
-app.options('/*', cors({ origin: true }));
 app.use(express.json());
 
 // API Routes
