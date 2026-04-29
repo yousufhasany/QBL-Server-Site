41a3792371b8b7b50ce2e57c5f7567c749f3add9
yousufhasany <hyousuf266@gmail.com>
2026-04-30 02:37:50 +0600

    Server error fixed!

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
-app.options('*', cors({ origin: true }));
+app.options('/*', cors({ origin: true }));
 app.use(express.json());
 
 // API Routes
